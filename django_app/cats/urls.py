aca89e836bf9972dbb3edd5c2c712ed02b298684
Hant0-0 <manoylo.maksimka@gmail.com>
2025-09-27 23:25:50 +0300

    added name url

--- django_app/cats/urls.py
+++ django_app/cats/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import CatListCreateAPIView, CatDetailAPIView
 
 urlpatterns = [
-    path("", CatListCreateAPIView.as_view()),
-    path("<int:cat_id>", CatDetailAPIView.as_view())
+    path("", CatListCreateAPIView.as_view(), name="cats-list"),
+    path("<int:cat_id>", CatDetailAPIView.as_view(), name="cat-detail")
 ]
--- django_app/mission/urls.py
+++ django_app/mission/urls.py
@@ -6,12 +6,12 @@ from .views import (ListCreateMissionAPIView, MissionDetailAPIView,
 from target.views import TargetDetailAPIView, TargetCompleteAPIView
 
 urlpatterns = [
-    path("", ListCreateMissionAPIView.as_view()),
-    path("<int:mission_id>", MissionDetailAPIView.as_view()),
-    path("<int:mission_id>/assign/", AssignMissionAPIView.as_view()),
-    path("<int:mission_id>/unassign/", UnassignMissionAPIView.as_view()),
+    path("", ListCreateMissionAPIView.as_view(), name="list-missions"),
+    path("<int:mission_id>", MissionDetailAPIView.as_view(), name="detail-mission"),
+    path("<int:mission_id>/assign/", AssignMissionAPIView.as_view(), name="assign-mission"),
+    path("<int:mission_id>/unassign/", UnassignMissionAPIView.as_view(), name="unassign-mission"),
 
     #TARGET
-    path("<int:mission_id>/targets/<int:target_id>/", TargetDetailAPIView.as_view()),
-    path("<int:mission_id>/targets/<int:target_id>/complete/", TargetCompleteAPIView.as_view()),
+    path("<int:mission_id>/targets/<int:target_id>/", TargetDetailAPIView.as_view(), name="detail-target"),
+    path("<int:mission_id>/targets/<int:target_id>/complete/", TargetCompleteAPIView.as_view(), name="complete-target"),
 ]
